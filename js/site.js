fa83738d302464e8ccade4c8f96ef1fd63c1c575
IanDerick <ian.derick@outlook.com>
2025-04-15 21:32:13 -0300

    main

--- js/site.js
+++ js/site.js
@@ -4,10 +4,18 @@ function togglePlay() {
   isPlaying = !isPlaying;
   document.getElementById('play-button').textContent = isPlaying ? '⏸️' : '▶️';
 }
+
+let isSound = false;
+
+function toggleSound(){
+  isSound = !isSound;
+  document.getElementById('sound-button').textContent = isSound ? '🔊': '🔇';
+}
+
 const range = document.getElementById('auto-range');
     let progress = 0;
     setInterval(() => {
       range.value = progress;
       progress++;
       if (progress > 100) progress = 0;
-    }, 1000); // ajusta a velocidade aqui (ms)
+    }, 1000);
